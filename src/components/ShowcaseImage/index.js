948fc7efcc7a304145431bc17cf9e6dd6d0887f9
Ty Streets <tstreets@iu.edu>
2024-10-16 10:40:43 -0400

    Added more styling the the showcase text area

--- src/components/ShowcaseImage/index.js
+++ src/components/ShowcaseImage/index.js
@@ -2,10 +2,14 @@ import showcaseStyles from "./showcase.module.css";
 
 export default function ShowcaseImage({ title, shortDesc }) {
   return (
-    <div>
-      <h4>{title}</h4>
-      <p>{shortDesc}</p>
-      <button className={showcaseStyles.showcaseButton}>See More</button>
+    <div
+      style={{ backgroundColor: "#a2c9d6", height: "300px", display: "flex" }}
+    >
+      <div className={showcaseStyles.showcaseText}>
+        <h4>{title}</h4>
+        <p>{shortDesc}</p>
+        <button className={showcaseStyles.showcaseButton}>See More</button>
+      </div>
     </div>
   );
 }
