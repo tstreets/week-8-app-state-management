3854ce3911b4cc77151ba0183a49c6edbd7cef96
Ty Streets <tstreets@iu.edu>
2024-10-23 10:38:35 -0400

    Added a form for adding a new peep
    Form using method created on useAppState for adding peeps

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,15 +1,46 @@
 "use client";
 import useAppState from "../../hooks/useAppState";
+import { useState } from "react";
 
 export default function About() {
+  const [newPersonForm, setNewPersonForm] = useState({ name: "" });
+
   const appState = useAppState();
 
-  console.log(appState);
+  function changeFormField(e) {
+    setNewPersonForm({
+      ...newPersonForm,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  }
+
+  function submitNewPerson(e) {
+    e.preventDefault();
+    appState.addPerson(newPersonForm);
+    setNewPersonForm({ name: "" });
+  }
+
+  console.log(appState.people);
 
   return (
     <>
       <main>
         <h1>About</h1>
+
+        <form onSubmit={submitNewPerson}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              name="name"
+              value={newPersonForm.name}
+              onChange={changeFormField}
+            />
+          </div>
+          <div>
+            <input type="submit" value="Add Person" />
+          </div>
+        </form>
       </main>
     </>
   );
